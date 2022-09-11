f2a0427eeb2ec22c4607dc79054a0e8563f10898
DaHerdi <danendra.herdian1243@gmail.com>
2022-09-11 14:12:15 +0700

    Inisiasi Tugas Lab 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
+from socketserver import DatagramRequestHandler
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+        data_catalog = CatalogItem.objects.all()
+        DatagramRequestHandler = {
+                'list_catalog' : data_catalog,
+                'nama' : 'Danendra Herdiansyah' 
+        }
+
+
+        return render(request, "katalog.html", DatagramRequestHandler)
