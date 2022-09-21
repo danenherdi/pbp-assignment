cee23fd4efc3bf6e385d687b5c6b1a5172a89b72
DaHerdi <danendra.herdian1243@gmail.com>
2022-09-21 15:14:00 +0700

    Mengubah model dan template mywatchlist

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class MyWatchlistItem(models.Model):
     status_watched_film = models.BooleanField()
     title_film = models.CharField(max_length=50)
-    rating_film = models.IntegerField()
+    rating_film = models.CharField(max_length=50)
     release_date_film = models.TextField()
     review_film = models.TextField()
